2e1586317442da7cbff7709cd1037e0eed1e1a8a
reyzory <kirillpanov1605@gmail.com>
2025-08-12 15:18:06 +0200

    logic of gates

--- amogusBall/Assets/Scripts/ball.cs
+++ amogusBall/Assets/Scripts/ball.cs
@@ -7,9 +7,19 @@ public class Ball : NetworkBehaviour
 {
     private NetworkRigidbody2D _networkRb;
 
+    [Networked] private Vector2 NetworkStartPosition { get; set; }
+
     private void Awake()
     {
         _networkRb = GetComponent<NetworkRigidbody2D>();
+    }
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            NetworkStartPosition = transform.position; // запоминаем стартовую позицию
+        }
     }
 
     public void Kick(Vector2 direction, float force)
@@ -27,5 +37,23 @@ public class Ball : NetworkBehaviour
             float damping = Mathf.Pow(0.99f, Runner.DeltaTime * 60f);
             _networkRb.Rigidbody.linearVelocity *= damping;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!HasStateAuthority)
+            return;
+
+        if (other.CompareTag("Goal"))
+        {
+            ResetBallPositionRPC();
+        }
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void ResetBallPositionRPC()
+    {
+        transform.position = NetworkStartPosition;
+        _networkRb.Rigidbody.linearVelocity = Vector2.zero;
     }
 }
